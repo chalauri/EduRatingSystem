8193db4b68fc094458635dada17c5fa7754c4eee
bumbeishvili <kurtsikidze.tatia@gmail.com>
2015-12-20 09:08:34 +0400

    add front part to the app

---
+++ WebContent/scripts/routes.js
@@ -0,0 +1,29 @@
+
+EduRate.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/rating');
+
+    $stateProvider
+        .state('ratingSummary', {
+            url: '/rating-summary',
+            templateUrl: 'partials/ratingSummary.html'
+        })
+
+        .state('logIn', {
+            url: '/login',
+            templateUrl: 'partials/login.html'
+        })
+        .state('rateUni', {
+            url: '/rate',
+            templateUrl: 'partials/rate.html'
+        })
+        .state('uniRatingDetails', {
+            url: '/ratingDetails/:uniId',
+            templateUrl: 'partials/ratingDetails.html',
+            controller:"ratingDetailsCtrl as _ratingDetail"
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: 'partials/register.html'
+        });
+
+}]);
